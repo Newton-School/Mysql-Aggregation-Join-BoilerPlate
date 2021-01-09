118b558837317041a875e38bd32293707c4a42c7
Sushil Kumar <rpsushil00@gmail.com>
2021-01-09 15:01:41 +0530

    grammar fix

--- src/sql/7.sql
+++ src/sql/7.sql
@@ -1,1 +1,1 @@
--- Query all the flights going to 'Paris' having duration is more than 500 from flights table
+-- Query all the flights going to 'Paris' having duration more than 500 from flights table
